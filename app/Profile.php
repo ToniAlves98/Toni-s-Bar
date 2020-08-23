990bc559534cc517a7bc2a2c266594517400067c
ToniAlves <57574403+ToniAlves@users.noreply.github.com>
2020-08-23 03:04:14 +0100

    final update

---
+++ app/Profile.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+    protected $guarded = [];
+
+    public function profileImage()
+    {
+        $imagePath = ($this->image) ? $this->image : 'profile/ZcA8bQKoTfYRYb2bNWRIr4LpwHUpWjJm8hrR6TKx.png';
+
+        return '/storage/' . $imagePath;
+    }
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ resources/views/email/welcome-email.blade.php
@@ -0,0 +1,10 @@
+@component('mail::message')
+# Welcome to Toni's Bar
+
+This is a community beer lovers.
+Share your photos
+
+
+All the best,<br>
+Toni.
+@endcomponent
